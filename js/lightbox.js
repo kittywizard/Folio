f63c0d8c8b1b3b702fc3bef41ed1e300f08a3a90
marleigh <volcana@gmail.com>
2018-10-01 21:01:16 -0700

    Beginning to code the modal lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,14 @@
+// important notes : use display:none to code lightbox in the html and then hide it
+
+console.log("test");
+
+//event listener for when someone clicks on a image
+
+//event listener for when somsone clicks on one of the arrows
+    //best way to handle.. everything is the same except if it's forward or backwards
+
+const leftArrow = document.getElementById('left');
+
+leftArrow.addEventListener("click", function() {
+    console.log("clicked");
+});
